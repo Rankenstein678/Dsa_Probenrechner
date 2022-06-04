db2b5271472e2f14da93dd8d983d96402d7aeed5
Rankenstein678 <94004557+Rankenstein678@users.noreply.github.com>
2022-06-04 17:02:02 +0200

    ChanceLogic Optimierung

--- app/src/main/java/com/rankenstein/dsahelper/logic/ChanceLogic.java
+++ app/src/main/java/com/rankenstein/dsahelper/logic/ChanceLogic.java
@@ -2,11 +2,11 @@ package com.rankenstein.dsahelper.logic;
 
 public class ChanceLogic {
     /**
-     * @param eig1  Eigenschaft 1
-     * @param eig2  Eigenschaft 2
-     * @param eig3  Eigenschaft 3
-     * @param taw Talentwert
-     * @param mod Modifikator (Erleichterung/Erschwernis)
+     * @param eig1 Eigenschaft 1
+     * @param eig2 Eigenschaft 2
+     * @param eig3 Eigenschaft 3
+     * @param taw  Talentwert
+     * @param mod  Modifikator (Erleichterung/Erschwernis)
      * @return [0]:Chance, [1]:durchschnittliche TaP*
      */
     public static double[] calculateChance(int eig1, int eig2, int eig3, int taw, int mod) {
@@ -20,17 +20,9 @@ public class ChanceLogic {
             int tap1 = tap; //bearbeitbarer TaW Zwischenstand
 
             //Gleicht Differenz zwischen Würfelergebnis und Eigenschaft mit TaW aus und verringert TaP dementsprechend
-            if (erg1 > eig1 && tap1 > 0) {
-                if (erg1 - tap1 <= eig1) {
-                    if (erg1 - eig1 < tap1) {
-                        int unmodifiedValue = erg1;
-                        erg1 -= erg1 - eig1;
-                        tap1 -= unmodifiedValue - eig1;
-                    } else {
-                        erg1 -= tap1;
-                        tap1 = 0;
-                    }
-                }
+            if (erg1 > eig1 && tap1 > 0 && erg1 - tap1 <= eig1) {
+                tap1 -= erg1 - eig1;
+                erg1 -= erg1 - eig1;
             }
 
             for (int w2 = 1; w2 <= 20; w2++) {
@@ -38,17 +30,9 @@ public class ChanceLogic {
                 int tap2 = tap1; //bearbeitbarer TaW Zwischenstand
 
                 //Gleicht Differenz zwischen Würfelergebnis und Eigenschaft mit TaP aus
-                if (erg2 > eig2 && tap2 > 0) {
-                    if (erg2 - tap2 <= eig2) {
-                        if (erg2 - eig2 < tap2) {
-                            int unmodifiedValue = erg2;
-                            erg2 -= erg2 - eig2;
-                            tap2 -= unmodifiedValue - eig2;
-                        } else {
-                            erg2 -= tap2;
-                            tap2 = 0;
-                        }
-                    }
+                if (erg2 > eig2 && tap2 > 0 && erg2 - tap2 <= eig2) {
+                    tap2 -= erg2 - eig2;
+                    erg2 -= erg2 - eig2;
                 }
 
                 for (int w3 = 1; w3 <= 20; w3++) {
@@ -56,17 +40,9 @@ public class ChanceLogic {
                     int tap3 = tap2; //bearbeitbarer TaW Zwischenstand
 
                     //Gleicht Differenz zwischen Würfelergebnis und Eigenschaft mit TaP aus
-                    if (erg3 > eig3 && tap3 > 0) {
-                        if (erg3 - tap3 <= eig3) {
-                            if (erg3 - eig3 < tap3) {
-                                int unmodifiedValue = erg3;
-                                erg3 -= erg3 - eig3;
-                                tap3 -= unmodifiedValue - eig3;
-                            } else {
-                                erg3 -= tap3;
-                                tap3 = 0;
-                            }
-                        }
+                    if (erg3 > eig3 && tap3 > 0 && erg3 - tap3 <= eig3) {
+                        tap3 -= erg3 - eig3;
+                        erg3 -= erg3 - eig3;
                     }
 
                     //Auswertung
