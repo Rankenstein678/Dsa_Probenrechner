1f73d8835317759a534ed03e173fcdea4f6f56fd
Rankenstein678 <94004557+Rankenstein678@users.noreply.github.com>
2022-06-04 15:35:55 +0200

    verwirrenden Kommentar geändert

--- app/src/main/java/com/rankenstein/dsahelper/logic/ChanceLogic.java
+++ app/src/main/java/com/rankenstein/dsahelper/logic/ChanceLogic.java
@@ -11,60 +11,60 @@ public class ChanceLogic {
      */
     public static double[] calculateChance(int eig1, int eig2, int eig3, int taw, int mod) {
         int passed = 0; //Erfolgreiche Proben
-        int tap = 0; //Gesamt TaP*
+        int sumTaP = 0; //Gesamt TaP*
 
-        final int tawMod = taw - mod; //Talentwert inklusive Modifikator
+        final int tap = taw - mod; //Talentwert inklusive Modifikator
 
         for (int w1 = 1; w1 <= 20; w1++) {
-            int t1 = tawMod; //bearbeitbarer TaW Zwischenstand
-            int erg1 = tawMod < 0 ? w1 - tawMod : w1; //Erhöhe das Würfelergebnis bei negativem Talentwert (doppelte Negierung)
+            int erg1 = tap < 0 ? w1 - tap : w1; //Erhöhe das Würfelergebnis bei negativem Talentwert (doppelte Negierung)
+            int tap1 = tap; //bearbeitbarer TaW Zwischenstand
 
-            //Gleicht Differenz zwischen Würfelergebnis und Eigenschaft mit TaW aus und verringert TaW dementsprechend
-            if (erg1 > eig1 && t1 > 0) {
-                if (erg1 - t1 <= eig1) {
-                    if (erg1 - eig1 < t1) {
+            //Gleicht Differenz zwischen Würfelergebnis und Eigenschaft mit TaW aus und verringert TaP dementsprechend
+            if (erg1 > eig1 && tap1 > 0) {
+                if (erg1 - tap1 <= eig1) {
+                    if (erg1 - eig1 < tap1) {
                         int unmodifiedValue = erg1;
                         erg1 -= erg1 - eig1;
-                        t1 -= unmodifiedValue - eig1;
+                        tap1 -= unmodifiedValue - eig1;
                     } else {
-                        erg1 -= t1;
-                        t1 = 0;
+                        erg1 -= tap1;
+                        tap1 = 0;
                     }
                 }
             }
 
             for (int w2 = 1; w2 <= 20; w2++) {
-                int t2 = t1; //bearbeitbarer TaW Zwischenstand
-                int erg2 = tawMod < 0 ? w2 - tawMod : w2; //Erhöhe das Würfelergebnis bei negativem Talentwert (doppelte Negierung)
+                int erg2 = tap < 0 ? w2 - tap : w2; //Erhöhe das Würfelergebnis bei negativem Talentwert (doppelte Negierung)
+                int tap2 = tap1; //bearbeitbarer TaW Zwischenstand
 
-                //Gleicht Differenz zwischen Würfelergebnis und Eigenschaft mit TaW aus
-                if (erg2 > eig2 && t2 > 0) {
-                    if (erg2 - t2 <= eig2) {
-                        if (erg2 - eig2 < t2) {
+                //Gleicht Differenz zwischen Würfelergebnis und Eigenschaft mit TaP aus
+                if (erg2 > eig2 && tap2 > 0) {
+                    if (erg2 - tap2 <= eig2) {
+                        if (erg2 - eig2 < tap2) {
                             int unmodifiedValue = erg2;
                             erg2 -= erg2 - eig2;
-                            t2 -= unmodifiedValue - eig2;
+                            tap2 -= unmodifiedValue - eig2;
                         } else {
-                            erg2 -= t2;
-                            t2 = 0;
+                            erg2 -= tap2;
+                            tap2 = 0;
                         }
                     }
                 }
 
                 for (int w3 = 1; w3 <= 20; w3++) {
-                    int t3 = t2; //bearbeitbarer TaW Zwischenstand
-                    int erg3 = tawMod < 0 ? w3 - tawMod : w3; //Erhöhe das Würfelergebnis bei negativem Talentwert (doppelte Negierung)
+                    int erg3 = tap < 0 ? w3 - tap : w3; //Erhöhe das Würfelergebnis bei negativem Talentwert (doppelte Negierung)
+                    int tap3 = tap2; //bearbeitbarer TaW Zwischenstand
 
-                    //Gleicht Differenz zwischen Würfelergebnis und Eigenschaft mit TaW aus
-                    if (erg3 > eig3 && t3 > 0) {
-                        if (erg3 - t3 <= eig3) {
-                            if (erg3 - eig3 < t3) {
+                    //Gleicht Differenz zwischen Würfelergebnis und Eigenschaft mit TaP aus
+                    if (erg3 > eig3 && tap3 > 0) {
+                        if (erg3 - tap3 <= eig3) {
+                            if (erg3 - eig3 < tap3) {
                                 int unmodifiedValue = erg3;
                                 erg3 -= erg3 - eig3;
-                                t3 -= unmodifiedValue - eig3;
+                                tap3 -= unmodifiedValue - eig3;
                             } else {
-                                erg3 -= t3;
-                                t3 = 0;
+                                erg3 -= tap3;
+                                tap3 = 0;
                             }
                         }
                     }
@@ -72,7 +72,7 @@ public class ChanceLogic {
                     //Auswertung
                     if ((w1 == 1 && w2 == 1) || (w1 == 1 && w3 == 1) || (w2 == 1 && w3 == 1)) { //Doppel 1 = Erfolg
                         passed++;
-                        tap += taw;
+                        sumTaP += taw;
                         continue;
                     }
                     if ((w1 == 20 && w2 == 20) || (w1 == 20 && w3 == 20) || (w2 == 20 && w3 == 20)) { //Doppel 20 = Misserfolg
@@ -80,14 +80,14 @@ public class ChanceLogic {
                     }
                     if (eig1 >= erg1 && eig2 >= erg2 && eig3 >= erg3) { //normale Probe
                         passed++;
-                        tap += Math.min(t3, taw); //Falls TaP*>TaW addiere TaW Punkte
+                        sumTaP += Math.min(tap3, taw); //Falls TaP*>TaW addiere TaW Punkte
                     }
 
                 }
             }
         }
         double chance = passed / 8000.0;
-        double avrgTaP = tap * 1.0 / passed; //durchschnittliche TaP* werden nur aus den erfolgreichen Proben berechnet
+        double avrgTaP = sumTaP * 1.0 / passed; //durchschnittliche TaP* werden nur aus den erfolgreichen Proben berechnet
         return new double[]{chance, avrgTaP};
     }
 }
